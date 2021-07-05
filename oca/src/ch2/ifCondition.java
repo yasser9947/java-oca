26b454ebcbf5662910f93810bdd8774c8e50e337
yasser9947 <yasser9946.1@gmail.com>
2021-07-05 10:07:56 +0300

    add the ch2

---
+++ oca/src/ch2/ifCondition.java
@@ -0,0 +1,14 @@
+package ch2;
+
+public class ifCondition {
+    // we will do it later
+    public static void main(String ...arg){
+
+        if ("abc" == "abc"){
+
+        }else {
+
+        }
+
+    }
+}
--- oca/src/ch3/FormattingDates.java
+++ oca/src/ch3/FormattingDates.java
@@ -8,7 +8,7 @@ public class FormattingDates {
     public static void main(String ...arg){
 
         LocalDate date = LocalDate.of(2020 , 2, 5);
-        System.
+//        System.
     }
 
 
