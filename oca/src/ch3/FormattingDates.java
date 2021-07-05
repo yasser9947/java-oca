54d0f907b26b26500ade45ed3730794004acc7de
yasser9947 <yasser9946.1@gmail.com>
2021-07-05 10:03:10 +0300

    end of ch3

--- oca/src/ch3/FormattingDates.java
+++ oca/src/ch3/FormattingDates.java
@@ -1,7 +1,15 @@
 package ch3;
 
+import java.time.LocalDate;
+
 public class FormattingDates {
 
+
+    public static void main(String ...arg){
+
+        LocalDate date = LocalDate.of(2020 , 2, 5);
+        System.
+    }
 
 
 }
